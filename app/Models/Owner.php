7df6172ebbf44543620073d3c2fe06977d5d0904
Sophie Stammers <sophie.stammers@gmail.com>
2020-11-11 11:23:27 +0000

    fullName and fullAddress functions added to Owner.php

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Owner extends Model
 {
     use HasFactory;
+
+    public function fullName()
+    {    
+        return $this->first_name . " " . $this->last_name;   
+    }
+
+    public function fullAddress()
+    {   
+        $addressString = "";
+
+        if ($this->address_1) {
+            $addressString .= $this->address_1 . ", ";     
+        }
+        if ($this->address_2) {
+            $addressString .= $this->address_2 . ", ";      
+        }
+        if ($this->town) {
+            $addressString .= $this->town . ", ";      
+        }
+        if ($this->postcode) {
+            $addressString .= $this->postcode;     
+        }
+
+        return $addressString;
+
+        // return "{$this->address_1}, {$this->address_2}, {$this->town}, {$this->postcode}";   
+    }
 }
