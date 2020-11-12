bf5cb9b7d187f1cee32c11b73b2d0b0f27eaf2ca
Sophie Stammers <sophie.stammers@gmail.com>
2020-11-12 17:09:48 +0000

    Owners retrieved and listed in welcome.blade.php

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer>
+<footer class="mt-4" class="page-footer">
     <span>&#128018</span>
     <span>&#129421</span>
     <span>&#128054</span>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,16 @@
 
 @section("content")
     <div class="list-group">
-        <a href="#" class="list-group-item list-group-item-action">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">Welcome to Happy Paws Vets Practice</h5>
-                <small>1 Day Ago</small>
-            </div>
-            <p class="mb-1">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-        </a>
+
+        @foreach (App\Models\Owner::all() as $owner)
+            <a href="/models/{{ $owner->id }}" class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">{{ $owner->fullName() }}</h5>
+                    <small>1 Day Ago</small>
+                </div>
+                <p class="mb-1">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </a>
+        @endforeach    
+
     </div>
 @endsection
