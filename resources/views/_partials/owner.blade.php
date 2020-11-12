16c91fe62c388d2ee5b7c8b1abe0f429acbfd164
Sophie Stammers <sophie.stammers@gmail.com>
2020-11-12 17:12:34 +0000

    Owners markup in partials

---
+++ resources/views/_partials/owner.blade.php
@@ -0,0 +1,7 @@
+<a href="/models/{{ $owner->id }}" class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">{{ $owner->fullName() }}</h5>
+        <small>1 Day Ago</small>
+    </div>
+    <p class="mb-1">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+</a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,8 @@
     <div class="list-group">
 
         @foreach (App\Models\Owner::all() as $owner)
-            <a href="/models/{{ $owner->id }}" class="list-group-item list-group-item-action">
-                <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">{{ $owner->fullName() }}</h5>
-                    <small>1 Day Ago</small>
-                </div>
-                <p class="mb-1">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            </a>
-        @endforeach    
+            @include("_partials/owner")
+        @endforeach            
 
     </div>
 @endsection
