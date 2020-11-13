44f2f19018072b3c362374c650eb1b1bd1d4414d
Sophie Stammers <sophie.stammers@gmail.com>
2020-11-13 17:44:40 +0000

    show method displays relevant info for the specified owner ID

--- resources/views/owner.blade.php
+++ resources/views/owner.blade.php
@@ -5,6 +5,15 @@
         <h2 class="card-header">Owner</h2>
         <article class="card-body">
             {{ $owner->fullName() }}
+        </article>        
+        <article class="card-body">
+            {{ $owner->fullAddress() }}
+        </article>
+        <article class="card-body">
+            {{ $owner->formattedPhoneNumber() }}
+        </article>
+        <article class="card-body">
+            {{ $owner->email }}
         </article>
     </div>
 @endsection
