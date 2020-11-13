6601f4bd45950cff15224957e5dab17d35418321
Sophie Stammers <sophie.stammers@gmail.com>
2020-11-13 10:41:34 +0000

    Owner and about routes added

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends("app")
+
+@section("content")
+    <div class="card">
+        <h2 class="card-header">About</h2>
+        <article class="card-body">
+            <p>Happy Pets About</p>
+            <p>lalala lalal alal alallallal a</p>
+        </article>
+    </div>
+@endsection
---
+++ resources/views/owner.blade.php
@@ -0,0 +1,10 @@
+@extends("app")
+
+@section("content")
+    <div class="card">
+        <h2 class="card-header">Owner</h2>
+        <article class="card-body">
+            <p>Some text lalala</p>
+            </article>
+    </div>
+@endsection
