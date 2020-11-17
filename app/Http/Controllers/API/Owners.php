8b4d10eefc18d315af04da3c389c0882283d6244
Sophie Stammers <sophie.stammers@gmail.com>
2020-11-17 11:06:30 +0000

    validation added to API

---
+++ app/Http/Controllers/API/Owners.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Owner;
+use App\Http\Requests\API\OwnerRequest;
+
+class Owners extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Owner::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(OwnerRequest $request)
+    {        
+        $data = $request->all();
+        return Owner::create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Owner $owner) //Route model binding
+    {
+        return $owner;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(OwnerRequest $request, Owner $owner)
+    {
+        $data = $request->all();
+        $owner->fill($data)->save();
+        return $owner;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Owner $owner)
+    {
+        $owner->delete();
+        return response(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Owners;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/owners", [Owners::class, "index"]); // responds to GET requests that comes from /api/owners
+
+Route::get("/owners/{owner}", [Owners::class, "show"]); // responds to GET requests that comes from /api/owners/{id}  
+
+Route::delete("/owners/{owner}", [Owners::class, "destroy"]); // responds to DELETE requests that comes from /api/owners/{id}  , returns a 204
+
+Route::post("/owners", [Owners::class, "store"]); // responds to POST requests that comes from /api/owners/{id}
+
+Route::put("/owners/{owner}", [Owners::class, "update"]); // responds to PUT to POST requests that comes from /api/owners/{id}
+
