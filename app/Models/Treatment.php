a477035d2965df8fdaba691b5c3e89bd9a077258
Sophie Stammers <sophie.stammers@gmail.com>
2020-11-18 16:26:58 +0000

    Method to turn treatment string to a tidy array of nonduplicated elems in Treatment model

--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -16,4 +16,13 @@ class Treatment extends Model
     // treatments have a many to many relationship with animals
     return $this->belongsToMany(Animal::class);
     }
+
+    public static function fromStrings(array $strings) : Collection
+    {
+        // Take an array of strings and turn it into a Collection. Use map to remove any whitespace from either side of each string. Unique to make sure that there aren’t any duplicate strings. Use the Model’s firstOrCreate method to find a matching Treatment in the database or create a new one if one doesn’t exist. Returns a collection containing all the relevant Treatment models from the databas
+        return collect($strings)->map(fn($str) => trim($str))
+        ->unique()
+        ->map(fn($str) => Treatment::firstOrCreate(["name" => $str]));       
+    }
+
 }
