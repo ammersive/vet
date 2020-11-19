128c7ce441ca7db4b95d2ee185357fc4b3a6ec67
Sophie Stammers <sophie.stammers@gmail.com>
2020-11-19 18:47:06 +0000

    TDD/ Cracker with unit test and this key word as less set up this way

--- app/Cracker.php
+++ app/Cracker.php
@@ -2,20 +2,45 @@
 
 namespace App;
 
-// Given an alphabet decryption key like the one below, create a program that can crack any message using the decryption key.
-// Alphabet:
-// a b c d e f g h i j k l m n o p q r s t u v w x y z
-
-// Decryption Key:
-// ! ) # ( . * % & > < @ a b c d e f g h i j k l m n o
+// Given an alphabet decryption key create a program that can crack any message using the decryption key.
 
 Class Cracker
 {
-    private $char1;
-    private $char2;
 
-    public function decrypt($char1)
+    private $message;
+    
+    // Attempt 4
+    public function decrypt($message)
     {   
-        return "a";
+        $alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+        $key =      "! ) # ( . * % & > < @ a b c d e f g h i j k l m n o";        
+        $cipher = array_combine(explode(" ", $key), explode(" ", $alphabet));
+        $cipher[" "] = " ";
+
+        $cracked = "";
+        $messageArray = str_split($message);
+    
+        foreach ($messageArray as $char) {
+            $cracked .= $cipher[$char];    
+        } 
+        return $cracked;
     }
 }
+
+    // // Attempt 3
+    // // The above but without space value added to $cipher
+
+    // // Attempt 2
+    // public function decrypt($char)
+    // {   
+    //     if ($char === ")") {
+    //         return "b";
+    //     }
+    //     return "a";
+    // }
+
+    // // Attempt 1
+    // public function decrypt($char)
+    // {   
+    //     return "a";
+    // }
--- tests/Unit/CrackerTest.php
+++ tests/Unit/CrackerTest.php
@@ -14,7 +14,7 @@ class CrackerTest extends TestCase
      */
     
     private $cracker;
-
+    
     public function setUp() : void
     {
         $this->cracker = new Cracker();
@@ -25,17 +25,75 @@ class CrackerTest extends TestCase
         $this->assertSame("a", $this->cracker->decrypt("!"));
     }
 
-    // public function test2()
-    // {
-    //     $this->assertSame("a", $this->cracker->decrypt("!"));
-    // }
+    public function test2()
+    {
+        $this->assertSame("b", $this->cracker->decrypt(")"));
+    }
 
+    public function test3()
+    {
+        $this->assertSame("ab", $this->cracker->decrypt("!)"));
+    }
 
+    public function test4()
+    {
+        $this->assertSame("krz", $this->cracker->decrypt("@go"));
+    }
 
-    // // Final test:
-    // public function testFull()
-    // {
-    //     $cracker = new Cracker("! ) # ( . * % & > < @ a b c d e f g h i j k l m n o");
-    //     $this->assertSame("hello mum", $cracker->decrypt("&.aad bjb"));
-    // }
+    // Final test:
+    public function testFull()
+    {
+        $cracker = new Cracker("! ) # ( . * % & > < @ a b c d e f g h i j k l m n o");
+        $this->assertSame("hello mum", $cracker->decrypt("&.aad bjb"));
+    }
 }
+
+
+
+
+
+
+// <?php
+
+// namespace Tests\Unit;
+
+// use PHPUnit\Framework\TestCase;
+// use App\Cracker;
+
+// class CrackerTest extends TestCase
+// {
+//     /**
+//      * A basic unit test example.
+//      *
+//      * @return void
+//      */
+    
+//     private $cracker;
+    
+//     public function setUp() : void
+//     {
+//         $this->cracker = new Cracker();
+//         $this->cracker->addAlphabet("a b c d e f g h i j k l m n o p q r s t u v w x y z");
+//         dd($this->cracker->alphabet);
+//         // $this->cracker->$addKey = "! ) # ( . * % & > < @ a b c d e f g h i j k l m n o";
+//     }
+
+//     public function test1()
+//     {
+//         $this->assertSame("a", $this->cracker->decrypt("!"));
+//     }
+
+//     public function test2()
+//     {
+//         $this->assertSame("b", $this->cracker->decrypt(")"));
+//     }
+
+//     // public function test3()
+//     // {
+//     //     $this->assertSame("ab", $this->cracker->decrypt("!)"));
+//     // }
+
+//     public function test4()
+//     {
+//         $this->assertSame("a b c d e f g h i j k l m n o p q r s t u v w x y z", $this->cracker->$alphabet);
+//     }
