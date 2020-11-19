f8896680b21e4fc4eb277ffaf263b8bc469b50c5
Sophie Stammers <sophie.stammers@gmail.com>
2020-11-19 19:13:47 +0000

    TDD/ Cracker class updated to accept key on construction so any 26 char key can be tested

--- app/Cracker.php
+++ app/Cracker.php
@@ -2,19 +2,23 @@
 
 namespace App;
 
-// Given an alphabet decryption key create a program that can crack any message using the decryption key.
+// Given an alphabet decryption key create a program that can crack any message using the decryption key given.
 
 Class Cracker
 {
 
     private $message;
+    private $key;
+
+    public function __construct($key)
+    {
+        $this->key = $key;
+    }
     
-    // Attempt 4
     public function decrypt($message)
     {   
-        $alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
-        $key =      "! ) # ( . * % & > < @ a b c d e f g h i j k l m n o";        
-        $cipher = array_combine(explode(" ", $key), explode(" ", $alphabet));
+        $alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z";               
+        $cipher = array_combine(explode(" ", $this->key), explode(" ", $alphabet));
         $cipher[" "] = " ";
 
         $cracked = "";
@@ -26,6 +30,9 @@ Class Cracker
         return $cracked;
     }
 }
+
+    // // Attempt 4
+    // // Have $key declared inside decrypt() method, so only that key can be used. With final attempt, key is passed to object on creation, so many keys can be used.
 
     // // Attempt 3
     // // The above but without space value added to $cipher
--- tests/Unit/CrackerTest.php
+++ tests/Unit/CrackerTest.php
@@ -17,7 +17,8 @@ class CrackerTest extends TestCase
     
     public function setUp() : void
     {
-        $this->cracker = new Cracker();
+        // can pass in any key of 26 chars separated by spaces
+        $this->cracker = new Cracker("! ) # ( . * % & > < @ a b c d e f g h i j k l m n o");
     }
 
     public function test1()
@@ -43,57 +44,6 @@ class CrackerTest extends TestCase
     // Final test:
     public function testFull()
     {
-        $cracker = new Cracker("! ) # ( . * % & > < @ a b c d e f g h i j k l m n o");
-        $this->assertSame("hello mum", $cracker->decrypt("&.aad bjb"));
+        $this->assertSame("hello mum", $this->cracker->decrypt("&.aad bjb"));
     }
 }
-
-
-
-
-
-
-// <?php
-
-// namespace Tests\Unit;
-
-// use PHPUnit\Framework\TestCase;
-// use App\Cracker;
-
-// class CrackerTest extends TestCase
-// {
-//     /**
-//      * A basic unit test example.
-//      *
-//      * @return void
-//      */
-    
-//     private $cracker;
-    
-//     public function setUp() : void
-//     {
-//         $this->cracker = new Cracker();
-//         $this->cracker->addAlphabet("a b c d e f g h i j k l m n o p q r s t u v w x y z");
-//         dd($this->cracker->alphabet);
-//         // $this->cracker->$addKey = "! ) # ( . * % & > < @ a b c d e f g h i j k l m n o";
-//     }
-
-//     public function test1()
-//     {
-//         $this->assertSame("a", $this->cracker->decrypt("!"));
-//     }
-
-//     public function test2()
-//     {
-//         $this->assertSame("b", $this->cracker->decrypt(")"));
-//     }
-
-//     // public function test3()
-//     // {
-//     //     $this->assertSame("ab", $this->cracker->decrypt("!)"));
-//     // }
-
-//     public function test4()
-//     {
-//         $this->assertSame("a b c d e f g h i j k l m n o p q r s t u v w x y z", $this->cracker->$alphabet);
-//     }
